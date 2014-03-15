808a6d93f3e5c0d80153a24dcbf59f6bc4fbab8c
ganchito55 <ganchito55@gmail.com>
2014-03-15 17:32:00 +0100

    Update temp.sh
    Only using shell script without C and you don't need compilete it.

--- temp.sh
+++ temp.sh
@@ -1,6 +1,10 @@
-
+#Version 2,0
+while test 0 -eq 0
+do
+sleep $1
 temp=$(/opt/vc/bin/vcgencmd measure_temp | cut -c 6-9) #know the temperature
 time=$(date +"%x" ) #dd/mm/yyyy
 time2=$(date +"%X") #hh:mm:ss
 echo "$time $time2*$temp" >> temp.txt
+done
 exit 0
